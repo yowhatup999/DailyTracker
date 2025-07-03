db0382b6b880dc1e317cfdff9d02de16b2fc1bf4
yowhatup999 <mark-skorupa@outlook.de>
2025-07-03 08:44:20 +0200

    Small changes to Deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8080/api'; // local usage
+// const API_BASE = 'http://localhost:8080/api'; // local usage
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+
 
 const api = axios.create({
     baseURL: API_BASE,
